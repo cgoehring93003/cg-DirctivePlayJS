30376786924ab52afd7a1e94e70c98ea259cac4a
cgoehring93003 <cgoehringgit@gmail.com>
2017-09-13 08:33:35 -0500

    first commit

--- app/formDirectives.js
+++ app/formDirectives.js
@@ -5,7 +5,28 @@ myApp.directive("myTable", function() {
     return {
         restrict: 'AE',		
 		replace: true,
-        templateUrl : 'templates/myTableTemplate.html'
+		// scope: {
+			// records: '=',
+		// },
+        templateUrl : 'templates/myTableTemplate.html',
+            link: function ($scope, element, attrs, fn) {
+
+				var tableColumns =  attrs['cols'];
+			
+			
+                // $scope.$watch('records', function () {
+
+                    // if (!$scope.records.length) {
+                        // $scope.empty = true;
+                        // return;
+                    // }
+					// tableColumns = tableColumns.replace(", ", ",");
+					// $scope.keys = tableColumns.split(',');
+
+                    // //$scope.keys = Object.keys($scope.tableData[0]);
+                // });
+
+            }		
 
     };
 });		
